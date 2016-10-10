7eb26050d0d98488da93d676cbf85bd84d3529ec
Andrew Beers <andybeers@gmail.com>
2016-10-10 16:39:23 -0700

    Added basic twit call

--- scripts/models/twitter.js
+++ scripts/models/twitter.js
@@ -1,15 +1,15 @@
-var Twit = require('twit');
-
-var T = new Twit({
-  consumer_key:         CONSUMER_KEY,
-  consumer_secret:      CONSUMER_SECRET,
-  access_token:         ACCESS_TOKEN,
-  access_token_secret:  ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
-
-T.get('search/tweets',
-  {q: 'banana', count: 10},
-  function(err, data, response) {
-    console.log(data);
-  });
+// var Twit = require('twit');
+//
+// var T = new Twit({
+//   consumer_key:         CONSUMER_KEY,
+//   consumer_secret:      CONSUMER_SECRET,
+//   access_token:         ACCESS_TOKEN,
+//   access_token_secret:  ACCESS_TOKEN_SECRET,
+//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+// });
+//
+// var requestTwitterProxy = T.get('search/tweets',
+//   {q: 'banana', count: 10},
+//   function(err, data) {
+//     response.json(data);
+//   });
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express'),
   requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
-  app = express();
+  app = express(),
+  Twit = require('twit');
+
+var twitterProxy = function(request, response) {
+  var T = new Twit({
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  });
+  T.get('search/tweets', {q: 'banana', count: 10},
+  function(err, data) {
+    response.json(data);
+  });
+};
 
 app.use(express.static('./'));
+
+app.get('/tweets', twitterProxy);
 
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
