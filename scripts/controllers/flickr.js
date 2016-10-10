416c4607231d5c18f7913f74872d70b52ece69f9
elhendricks <elhendricks@gmail.com>
2016-10-10 16:34:42 -0700

    wrote flickr.js, added flickrResponseProxy in server.js, logs out object returned from flickr api

---
+++ scripts/controllers/flickr.js
@@ -0,0 +1,22 @@
+(function(module) {
+
+  var flickrData = {};
+
+  var parkName = 'yosemite';
+
+  flickrData.fetchData = function() {
+    $.ajax({
+      type: 'GET',
+      url: '/flickr/',
+      url: '/flickr/?method=flickr.photos.search&format=json&tags=' + parkName,
+      success: function(data){
+        console.log(data);
+      }
+    });
+  };
+
+  module.flickrData = flickrData;
+
+}(window));
+
+flickrData.fetchData();
