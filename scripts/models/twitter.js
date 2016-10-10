e4083f0fab45b6fcad21bb390fdf1f4393ee0a0a
Andrew Beers <andybeers@gmail.com>
2016-10-10 14:58:36 -0700

    Added hard-coded tokens.json file, also added tokens to gitignore.

--- scripts/models/twitter.js
+++ scripts/models/twitter.js
@@ -1,1 +1,9 @@
 var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         CONSUMER_KEY,
+  consumer_secret:      CONSUMER_SECRET,
+  access_token:         ACCESS_TOKEN,
+  access_token_secret:  ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
