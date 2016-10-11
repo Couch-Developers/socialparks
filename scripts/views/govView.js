6444054298e137256ce682294ee0905d62f67f38
sdcaulley <stephanie.caulley@gmail.com>
2016-10-11 11:12:04 -0700

    state select is populating

--- scripts/views/govView.js
+++ scripts/views/govView.js
@@ -6,9 +6,18 @@
     var parksNameArray = JSON.parse(localStorage.getItem('parkNames'));
     var statesArray = [];
     parksNameArray.forEach(function(obj) {
-      console.log(obj);
-      statesArray.push(obj.states);
+      statesArray.push(obj.state);
     });
+    var myout = statesArray.reduce(function(prev, next) {
+      console.log(statesArray);
+      prev[next] = (prev[next] || 0) + 1;
+      return prev;
+    }, {});
+    for (keys in myout) {
+      this.keys = myout.keys;
+      var optionTag = '<option value="' + keys + '">' + keys + ' ' + myout[keys] + '</option>';
+      $('#state-filter').append(optionTag);
+    }
   };
 
   module.parksView = parksView;
