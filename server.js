ba7382eb184fa127f6d17cdb721054f14b0ef0ff
Andrew Beers <andybeers@gmail.com>
2016-10-10 18:13:31 -0700

    Renamed twitterProxy to proxyTwitter to match other methods

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express'),
   app = express(),
   Twit = require('twit');
 
-var twitterProxy = function(request, response) {
+function proxyTwitter(request, response) {
+  console.log('Routing Twitter request for', request.params[0]);
   var T = new Twit({
     consumer_key:         process.env.CONSUMER_KEY,
     consumer_secret:      process.env.CONSUMER_SECRET,
@@ -14,20 +15,19 @@ var twitterProxy = function(request, response) {
   });
   T.get('search/tweets', {q: request.params[0], count: 10},
   function(err, data) {
-    console.log(data);
     response.json(data);
   });
 };
 
-  function proxyNpsApi(request, response) {
-    console.log('Routing NPS request for', request.params[0]);
-    (requestProxy({
-      url: 'https://developer.nps.gov/api/v0/' + request.params[0],
-      headers: {Authorization: process.env.NPS_API}
-    }))(request, response);
-  };
+function proxyNpsApi(request, response) {
+  console.log('Routing NPS request for', request.params[0]);
+  (requestProxy({
+    url: 'https://developer.nps.gov/api/v0/' + request.params[0],
+    headers: {Authorization: process.env.NPS_API}
+  }))(request, response);
+};
 
-  app.get('/nps/*', proxyNpsApi);
+app.get('/nps/*', proxyNpsApi);
 
 function proxyFlickr(request, response) {
   console.log('Routing Flickr request for', request.params[0]);
@@ -46,7 +46,7 @@ app.get('/flickr/*', proxyFlickr);
 
 app.use(express.static('./'));
 
-app.get('/tweets/*', twitterProxy);
+app.get('/tweets/*', proxyTwitter);
 
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
