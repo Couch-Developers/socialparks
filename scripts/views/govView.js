f5a6f09045302498fdc79fda7bba875b1922ba39
elhendricks <elhendricks@gmail.com>
2016-10-12 14:57:03 -0700

    uncommented code

--- scripts/views/govView.js
+++ scripts/views/govView.js
@@ -38,20 +38,20 @@
 
   parksView.handleParksFilter = function() {
     $('#park-filter').on('change', function() {
-      // if ($(this).val()) {
-      //   var codeValue = $(this).val();
-      //   $.ajax({
-      //     url: '/nps/parks?fields=addresses%2Ccontacts%2CentranceFees%2CentrancePasses%2Cimages%2CoperatingHours&parkCode=' + codeValue,
-      //     success: function(data) {
-      //       completeData = data;
-      //       $('#gov-data').append(parksObj.toHtml(completeData));
-      //     }
-      //   });
-      //
-      // }
+      if ($(this).val()) {
+        var codeValue = $(this).val();
+        $.ajax({
+          url: '/nps/parks?fields=addresses%2Ccontacts%2CentranceFees%2CentrancePasses%2Cimages%2CoperatingHours&parkCode=' + codeValue,
+          success: function(data) {
+            completeData = data;
+            $('#gov-data').append(parksObj.toHtml(completeData));
+          }
+        });
+
+      }
     });
   };
-  //
+  
 
   parksView.navigateFromParksFilter = function(ctx, next) {
     $('#park-filter').on('change', function() {
