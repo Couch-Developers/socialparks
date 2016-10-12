06b8f23524ce6d8ff0e5bf4974647367ecd2a974
Steven Jackson <hpats67@gmail.com>
2016-10-12 15:16:30 -0700

    Started to make the population of the state page.

--- scripts/controllers/stateController.js
+++ scripts/controllers/stateController.js
@@ -6,6 +6,43 @@
     $('#state-page').fadeIn().siblings().hide();
   };
 
+  stateController.emptyHtml = function(ctx, next) {
+    $('#state-page').empty();
+    next();
+  };
+
+  stateController.fetchData = function(stateId, nextFunction) {
+    $.ajax({
+      type: 'GET',
+      url: '/nps/parks?fields=images&stateCode=' + stateId,
+      success: function(data) {
+        stateController.populateHandlebars(data);
+        nextFunction();
+      }
+    });
+  };
+
+  stateController.loadData = function(ctx, next) {
+    var id = states.map(function (data) {
+      return data.id;
+    });
+    stateController.fetchData(id[0], stateController.populateHandlebars);
+    next();
+  };
+
+  // Handlebars template
+  stateController.toHtml = function(obj) {
+    var template = Handlebars.compile($('#state-template').html());
+    return template(obj);
+  };
+
+  stateController.populateHandlebars = function (arr) {
+    // $('').append().before();
+    arr.forEach(function(obj) {
+      $('#state-page').append(stateController.toHtml(obj));
+    });
+  };
+
   module.stateController = stateController;
 
 }(window));
---
+++ scripts/models/state.js
@@ -0,0 +1,9 @@
+(function(module) {
+
+  var stateModel = {};
+
+
+
+  module.state = stateModel;
+
+}(window));
