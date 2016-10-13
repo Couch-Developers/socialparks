548cb123dcc58216e957e7a61261a54808453aea
elhendricks <elhendricks@gmail.com>
2016-10-13 09:20:36 -0700

    modified form and javascript for form.  nothing works

--- scripts/views/compareView.js
+++ scripts/views/compareView.js
@@ -1,18 +1,16 @@
 (function(module){
 
+var compareForm = {};
 
+compareForm.submitForm = function(event) {
+  event.preventDefault();
+  var arr = []
+  arr.push($('form input').attr('checked'));
+  console.log(arr);
+}
 
+$('form').on('submit', compareForm.submitForm);
 
-
-
-
-
-
-
-
-
-
-
-
+module.compareForm = compareForm;
 
 })(window);
